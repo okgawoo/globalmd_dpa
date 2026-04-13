d5b4b7bc85cc672187641bdf8aaf7c25011ca55a
okgawoo <okgawoo@gmail.com>
2026-04-13 18:34:03 +0900

    푸시 알림 구독 연결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { supabase } from '../lib/supabase'
+import { subscribeToPush } from '../lib/pushSubscription'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -16,6 +17,14 @@ export default function App({ Component, pageProps }: AppProps) {
         router.replace('/login')
       }
       setChecking(false)
+
+      // 로그인 상태면 푸시 구독 시도
+      if (session?.user) {
+        supabase.from('dpa_agents').select('id').eq('user_id', session.user.id).single()
+          .then(({ data: agent }) => {
+            if (agent?.id) subscribeToPush(agent.id)
+          })
+      }
     })
 
     const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
